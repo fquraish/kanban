ac4d7e9c387a78398a2c59f0fdc0405e60465fc5
léni <lenipep@hotmail.fr>
2019-10-22 11:24:49 +0200

    o

---
+++ script.js
@@ -0,0 +1,45 @@
+
+$(document).ready(function () {
+
+    $('button').click(function () {
+        if ($('input').val() != "") {
+            var li = $('<li></li>')
+            
+            $('ol').append(li)
+            li.html($('#taskname').val())
+            var deleteLi = $("<span>   X</span>")
+            li.append(deleteLi)
+            $(deleteLi).click(function(){
+                li.remove()
+            })
+            
+            
+        }
+           $('input').val("")
+        
+
+    })
+
+
+    $('#items').sortable();
+
+    $(function(){
+       
+     
+        //with control
+        var timer1=$('.timer1').startTimer({
+            onComplete: function(element){console.log('Complete');}
+            ,onPause: $('.timerpause'),
+            onReset: $('.timerreset'),
+            onStart: $('.timerstart'),
+            onComplete: function(element){
+                var li = $('li')
+                $('#taskdone').append(li.first());
+              }
+        });
+        
+       
+    })
+
+})
+
